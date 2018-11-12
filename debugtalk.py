04f0786e5d4eee40981000da898fcd4a4b924113
GithubSlowWater <568033067@qq.com>
2018-11-12 10:34:24 +0800

    0.0.2
    api_script

--- debugtalk.py
+++ debugtalk.py
@@ -1,95 +1,97 @@
+import pymysql
+import os
+import random
+import string
 import json
 import re
-import csv
-import xlrd
-import xlwt
+# 引入环境变量
+backlogin = os.environ["backlogin"]           # admin
+backpwd = os.environ["backpwd"]           # wsy88888
+password = os.environ["password"]    # wsy888888
+env = os.environ["env"]           
+base_url = os.environ["base_url"]   
 
-x = {
-    "errcode": 0,
-    "errmsg": "操作成功",
-    "list_data": [
-        {
-            "user_id": "-1",
-            "card_num": "10000208",
-            "card_value": "100000000",
-            "stakeholder_name": "测试014",
-            "deadline": "2018-12-25",
-            "eq_integral_value": "0",
-            "received": "0",
-            "wait_release": "0",
-            "bindtime": "0000-00-00 00:00:00",
-            "card_pass": "03b9JyRyXhxzJqpRrk1r3HaAA7XPVS69o3N3ZBDGhhDvS9g",
-            "bind": "未绑定",
-            "valid": "2018\/12\/25",
-            "binddate": "-",
-            "op_btn": "<button class=\"td-btn primary\" onclick=\"copy('10000208')\">复制卡密<\/button>\n                            <input type=\"hidden\" value='权益卡卡号：10000208 密码：BGodxM，使用方法：登录APP-我的-我的权益卡内进行绑定即可' name='copy_10000208'>\n                            <button class=\"td-btn primary\" data-target=\"#changeTime\" data-toggle=\"modal\" onclick=\"getid('10000208')\">变更期限<\/button>\n                            <button class=\"td-btn primary\" onclick=\"window.location.href = '.\/index.php?m=equity&a=detail&card_num=10000208'\">详情<\/button>"
-        },
-        {
-            "user_id": "-1",
-            "card_num": "10000207",
-            "card_value": "100000000",
-            "stakeholder_name": "测试013",
-            "deadline": "2018-12-24",
-            "eq_integral_value": "0",
-            "received": "0",
-            "wait_release": "0",
-            "bindtime": "0000-00-00 00:00:00",
-            "card_pass": "da8bMnvmQohBp4uqwOrZSQWu2v65tC187\/1MpkUE1\/5aoEY",
-            "bind": "未绑定",
-            "valid": "2018\/12\/24",
-            "binddate": "-",
-            "op_btn": "<button class=\"td-btn primary\" onclick=\"copy('10000207')\">复制卡密<\/button>\n                            <input type=\"hidden\" value='权益卡卡号：10000207 密码：QoZfC9，使用方法：登录APP-我的-我的权益卡内进行绑定即可' name='copy_10000207'>\n                            <button class=\"td-btn primary\" data-target=\"#changeTime\" data-toggle=\"modal\" onclick=\"getid('10000207')\">变更期限<\/button>\n                            <button class=\"td-btn primary\" onclick=\"window.location.href = '.\/index.php?m=equity&a=detail&card_num=10000207'\">详情<\/button>"
-        }
-        ]
-        }
+# 随机整型数字
+def random_int(x,y=999999):
+    return random.randint(x,y)
 
-s = x['list_data'][0]['op_btn']
-print(s)
-result1 = re.findall('权益卡卡号：(\d+)', s)
-result2 = re.findall('密码：(\w+)', s)
-print(result1)
-print(result2)
-mapz = (result1 + result2)
-print(mapz)
+# 随机浮点型数字
+def random_float(x,y=999999,k=4):
+    return round(random.uniform(x,y),k)
+
+# 随机字符
+def random_str(x):
+	ran_str = ''.join(random.sample(string.ascii_letters + string.digits, x))
+	return ran_str
+
+# 相加
+def sum_two(x, y):
+	x = float(x)
+	y = float(y)
+	return x + y
+
+# 相乘
+def mult_two(x,y):
+	x = float(x)
+	y = float(y)
+	return x * y
 
 
-#测试xlwt
-#创建新的excel文件
-newfile = xlwt.Workbook()
-#创建新的表单
-#addsheet的格式add_sheet(sheetname, cell_overwrite_ok=False)
-newsheet = newfile.add_sheet('test1',cell_overwrite_ok=True)
-#索引从0，0开始
-newsheet.write(0,0,mapz[0])
-newsheet.write(0,1,mapz[1])
-newfile.save('firsttest.xls')
- 
-#打开相应的excel文件
-workbook = xlrd.open_workbook(r'firsttest.xls')
-#找到相应的sheet
-#可以通过index，index从0开始计算
-#也可以通过sheet的名字 
-rdsheet = workbook.sheet_by_index(0) 
-rdsheet = workbook.sheet_by_name('test1')
-print(rdsheet.cell(0,0))
-#cell函数返回的是Cell 对象）包含ctype value xf_index
-#输出number:10.0
-print(rdsheet.cell(0,0).ctype)
-#输出2？使用type查询结果为:<type 'member_descriptor'>具体含义？
-print(rdsheet.cell(0,0).value)
-#输出10.0
-print(rdsheet.cell(0,0).xf_index)
-#输出None
- 
-# rdbook = xlrd.open_workbook(r'firsttest.xls')
-# wtbook = copy.copy(rdbook)
-# wtsheet = wtbook.get_sheet(0)
-# type(wtsheet)
-# wtsheet.write(0,0,'aaaaa')
-# wtbook.save('bb.xls')
- 
-# newrdbook = xlrd.open_workbook(r'bb.xls')
-# print(newrdbook.sheet_by_name('test1').cell(0,0))
-# #输出text:'aaaaa'
-# print(rdbook.sheet_by_index(0).cell(0,0))
-# #输出number:10.0
+# 取出最小的币种ID
+def extract_change_rate(content_data):
+	data = json.dumps(content_data)
+	data_1 = json.loads(data)
+	# return data_1['data'][3]['change_rate']
+	l =[]
+	for x in range(len(data_1)):
+		s = data_1['data'][x]['change_rate']
+		l.append(s)
+	i = l.index(min(l))
+	return data_1['data'][i]['id']
+
+# 自动递增
+def count_num(x):
+	return x + 1
+
+# 抽取权益卡密码
+def card_pass(content_data):
+	x = re.findall("密码：(\w{6})", content_data)
+	return x[0]
+
+
+
+# #创建连接
+# conn = pymysql.connect(host="172.16.10.184", port=3313, user="root", password="tW2sDsVzD7Nae1jy",
+#                        db="wsy_blockchain", charset='utf8')
+# # 修改为888888
+# def insetsql():
+# 	cursor = conn.cursor()
+# 	sql = 'UPDATE rights_card SET card_pass = \'45bdm2I/LOuviIJIvxwJp3eQACqpRYxaQ7n4Ob0upf/9LAg\' ORDER BY card_num DESC LIMIT 1;'
+# 	try:
+# 		# 执行sql语句
+# 		cursor.execute(sql)
+# 		conn.commit()
+# 	except:
+# 		conn.rollback()
+# 		print("sql执行失败！")
+# 	finally:
+# 		cursor.close()
+# 		conn.close()
+# 		print("sql执行成功！")
+
+# #查询权益卡ID
+# def selecesql():
+# 	cursor = conn.cursor()
+# 	sql = 'SELECT * FROM rights_card ORDER BY card_num DESC LIMIT 1;'
+# 	try:
+# 		cursor.execute(sql)
+# 		row_1 = cursor.fetchone()
+# 	except:
+# 		conn.rollback()
+# 		print("sql执行失败！")
+# 	finally:
+# 		cursor.close()
+# 		conn.close()
+# 		print("sql执行成功！")
+# 	return row_1[0]
+
---
+++ report_mail.py
@@ -0,0 +1,69 @@
+# 发送html内容的邮件
+import smtplib, time, os
+from email.mime.text import MIMEText
+from email.header import Header
+from email.utils import formataddr
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+
+
+def send_mail_html(file):
+    '''发送html内容邮件'''
+    # 发送邮箱
+    sender = 'wsyceshi002@126.com'
+    # 接收邮箱
+    receiver = 'chenhuike@mail.weisanyun.com'
+    # 发送邮件主题
+    t = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    subject = '自动化测试结果_' + t
+    # 发送邮箱服务器
+    smtpserver = 'smtp.126.com'
+    # 发送邮箱用户/密码
+    username = 'wsyceshi002@126.com'
+    password = 'KINGCHEN123'
+
+    # 读取html文件内容
+    f = open(file, 'rb')
+    mail_body = f.read()
+    f.close()
+
+    # 组装邮件内容和标题，中文需参数‘utf-8’，单字节字符不需要
+    msg = MIMEText(mail_body, _subtype='html', _charset='utf-8')
+    msg['Subject'] = Header(subject, 'utf-8')
+    msg['From'] = sender
+    msg['To'] = receiver
+
+    # 附件信息
+    msg = MIMEMultipart('related')
+    attach = MIMEApplication(open(file , 'rb').read())
+    attach.add_header('Content-Disposition', 'attachment', filename= t + 'reports.html')
+    msg.attach(attach)
+
+    # 登录并发送邮件
+    try:
+        smtp = smtplib.SMTP()
+        smtp.connect(smtpserver)
+        smtp.login(username, password)
+        smtp.sendmail(sender, receiver, msg.as_string())
+    except:
+        print("邮件发送失败！")
+    else:
+        print("邮件发送成功！")
+    finally:
+        smtp.quit()
+
+def find_new_file(dir):
+    '''查找目录下最新的文件'''
+    file_lists = os.listdir(dir)
+    file_lists.sort(key=lambda fn: os.path.getmtime(dir + "/" + fn)
+                    if not os.path.isdir(dir + "/" + fn)
+                    else 0)
+    # print('最新的文件为： ' + file_lists[-1])
+    file = os.path.join(dir, file_lists[-1])
+    print('完整文件路径：', file)
+    return file
+
+
+dir = '/opt/api/jenkins/jobs/blockchain/reports'  # 指定文件目录
+file = find_new_file(dir)  # 查找最新的html文件
+send_mail_html(file)  # 发送html内容邮件
